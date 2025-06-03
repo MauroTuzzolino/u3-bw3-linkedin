e3a95b0bfa36562bbc82a0f1131454cd93c6461a
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-06-03 16:06:24 +0200

    Redux / Import
    Aggiunti i vari import per redux, inizio creazione dello store e del reducer per la fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,27 @@ import ProfileSection from "./components/ProfileSection";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { Container, Row, Col } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div className="page-wrapper">
-        <div className="page-content">
-          <NavBar />
-          <Container className="my-4">
-            <Row>
-              <Col xs={12} md={7} lg={9}>
-                <ProfileSection />
-              </Col>
-              <Col xs={0} md={5} lg={3}></Col>
-            </Row>
-          </Container>
+      <BrowserRouter>
+        <div className="page-wrapper">
+          <div className="page-content">
+            <NavBar />
+            <Container className="my-4">
+              <Row>
+                <Col xs={12} md={7} lg={9}>
+                  <ProfileSection />
+                </Col>
+                <Col xs={0} md={5} lg={3}></Col>
+              </Row>
+            </Container>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </BrowserRouter>
     </>
   );
 }
