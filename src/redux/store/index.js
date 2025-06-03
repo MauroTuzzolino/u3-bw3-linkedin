974b33fe15f99fd7f2b03ce66a0be59f2139a1b6
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-06-03 16:13:03 +0200

    Commit necessario

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,1 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import profileSlice from "../reducers/profileSlice";
+
+const store = configureStore({
+  reducer: {
+    profile: profileSlice,
+  },
+});
+
+export default store;
