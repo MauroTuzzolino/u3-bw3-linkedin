c8577bf4a903c95037b70153e1e0a263a5db26cd
ZairaStra <straticozaira@gmail.com>
2025-06-03 16:45:50 +0200

    infosections

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import ProfileSection from "./components/ProfileSection";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { Container, Row, Col } from "react-bootstrap";
+import InfoSections from "./components/InfoSections";
+import Graphic from "./assets/images/graphic.png";
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
             <Row>
               <Col xs={12} md={7} lg={9}>
                 <ProfileSection />
+                <InfoSections header="Informazioni" title="Nome Cognome" subtitle="Dettagli Personali" details="Lorem ipsum" image={Graphic} />
+                <InfoSections header="Esperienza" title="Mansione" subtitle="Tipo di lavoro" details="durata" image={Graphic} />
+                <InfoSections header="Formazione" title="Scuola/università" subtitle="durata" details="Votazione" image={Graphic} />
+                <InfoSections header="Competenze" title="Disciplina" subtitle="Scuola/università" details="Altre informazioni" image={Graphic} />
               </Col>
               <Col xs={0} md={5} lg={3}></Col>
             </Row>
---
+++ src/components/InfoSections.jsx
@@ -0,0 +1,40 @@
+import { Button, Card, Col, Row } from "react-bootstrap";
+import { PlusLg } from "react-bootstrap-icons";
+import { Pencil } from "react-bootstrap-icons";
+
+const InfoSections = ({ header, title, subtitle, details, image }) => {
+  return (
+    <Card className="my-2 py-3">
+      <Card.Header className="bg-white border-0">
+        <Row className="align-items-center">
+          <Col>
+            <h4>{header}</h4>
+          </Col>
+          <Col className="text-end">
+            <Button variant="light" className="border-0 bg-transparent">
+              <PlusLg className="me-3 " size={25} />
+            </Button>
+            <Button variant="light" className="border-0 bg-transparent">
+              <Pencil size={25} />
+            </Button>
+          </Col>
+        </Row>
+      </Card.Header>
+      <Card.Body>
+        <Row>
+          <Col md={1} className="d-none d-md-block">
+            <img src={image} alt="graphic" className="img-fluid" />
+          </Col>
+
+          <Col>
+            <h5>{title}</h5>
+            <h6>{subtitle}</h6>
+            <p>{details}</p>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default InfoSections;
