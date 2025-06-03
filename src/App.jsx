28014f5d37002d04d88b0a23bd08dca1acebfa40
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-06-03 12:23:24 +0200

    Creazione del componente NavBar, installazione delle icone di react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Container } from "react-bootstrap";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
 
 export default App;
