d238c04e471900244911751774af3bd483b44734
ZairaStra <straticozaira@gmail.com>
2025-06-04 12:13:09 +0200

    infosections spostate in profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import ProfileSection from "./components/ProfileSection";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { Container, Row, Col } from "react-bootstrap";
-import InfoSections from "./components/InfoSections";
-import Graphic from "./assets/images/graphic.png";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./assets/components/Sidebar";
 
@@ -18,10 +16,6 @@ function App() {
             <Row>
               <Col xs={12} md={7} lg={9}>
                 <ProfileSection />
-                <InfoSections header="Informazioni" title="Nome Cognome" subtitle="Dettagli Personali" details="Lorem ipsum" image={Graphic} />
-                <InfoSections header="Esperienza" title="Mansione" subtitle="Tipo di lavoro" details="durata" image={Graphic} />
-                <InfoSections header="Formazione" title="Scuola/università" subtitle="durata" details="Votazione" image={Graphic} />
-                <InfoSections header="Competenze" title="Disciplina" subtitle="Scuola/università" details="Altre informazioni" image={Graphic} />
               </Col>
               <Col xs={0} md={5} lg={3}>
                 <Sidebar />
--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Card, Col, Image, Row } from "react-bootstrap";
+import { Alert, Button, Card, Col, Image, Row, Spinner } from "react-bootstrap";
 import coverImage from "../assets/images/placeholderCover.png";
 import avatar from "../assets/images/avatar.svg";
 import linkSvg from "../assets/images/vite.svg";
@@ -7,6 +7,8 @@ import { Pen, Pencil } from "react-bootstrap-icons";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getMyProfile } from "../redux/actions/index";
+import InfoSections from "./InfoSections";
+import Graphic from "../assets/images/graphic.png";
 
 const ProfileSection = () => {
   const dispatch = useDispatch();
@@ -18,9 +20,9 @@ const ProfileSection = () => {
 
   if (loading) {
     return (
-      <div className="text-center my-5">
-        <p>Caricamento profilo...</p>
-      </div>
+      <Spinner animation="border" role="status" variant="info">
+        <span className="visually-hidden">Caricamento profilo...</span>
+      </Spinner>
     );
   }
   if (error) {
@@ -36,101 +38,107 @@ const ProfileSection = () => {
   }
 
   return (
-    <Card>
-      <div
-        style={{
-          position: "absolute",
-          zIndex: "1000",
-          top: "1rem",
-          right: "1rem",
-          backgroundColor: "#e0e0e0",
-          borderRadius: "50%",
-          padding: "12px",
-          display: "inline-flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <FaCamera size={17} fill="primary" />
-      </div>
-      <Card.Img variant="top" src={coverImage} className="coverImage position-relative" />
-      <Card.Body>
-        <Image src={profileSection.image || avatar} className="profileImg" alt="Profilo" />
-        {/* //mettere immagini matita con abosolute */}
-        <Row className="position-relative pt-5">
-          <Row>
-            <Col>
-              <h2>
-                {profileSection.name} {profileSection.surname}
-              </h2>
-              <h3>{profileSection.title}</h3>
-              <Row>
-                <Col xs={12} md={6}>
-                  <p className="text-muted">{profileSection.area}</p>
-                </Col>
-                <Col xs={12} md={6}>
-                  <p>{profileSection.email}</p>
-                </Col>
-              </Row>
-            </Col>
-            <Col className="d-flex justify-content-center align-items-center">
-              <div>
-                <div className="d-block mb-3">
-                  <Image src={linkSvg} />
-                  <h6 className="d-inline-block">AZIENDA</h6>
+    <>
+      <Card>
+        <div
+          style={{
+            position: "absolute",
+            zIndex: "1000",
+            top: "1rem",
+            right: "1rem",
+            backgroundColor: "#e0e0e0",
+            borderRadius: "50%",
+            padding: "12px",
+            display: "inline-flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <FaCamera size={17} fill="primary" />
+        </div>
+        <Card.Img variant="top" src={coverImage} className="coverImage position-relative" />
+        <Card.Body>
+          <Image src={profileSection.image || avatar} className="profileImg" alt="Profilo" />
+          {/* //mettere immagini matita con abosolute */}
+          <Row className="position-relative pt-5">
+            <Row>
+              <Col>
+                <h2>
+                  {profileSection.name} {profileSection.surname}
+                </h2>
+                <h3>{profileSection.title}</h3>
+                <Row>
+                  <Col xs={12} md={6}>
+                    <p className="text-muted">{profileSection.area}</p>
+                  </Col>
+                  <Col xs={12} md={6}>
+                    <p>{profileSection.email}</p>
+                  </Col>
+                </Row>
+              </Col>
+              <Col className="d-flex justify-content-center align-items-center">
+                <div>
+                  <div className="d-block mb-3">
+                    <Image src={linkSvg} />
+                    <h6 className="d-inline-block">AZIENDA</h6>
+                  </div>
+                  <div className="d-block">
+                    <Image src={linkSvg} />
+                    <h6 className="d-inline-block">AZIENDA</h6>
+                  </div>
                 </div>
-                <div className="d-block">
-                  <Image src={linkSvg} />
-                  <h6 className="d-inline-block">AZIENDA</h6>
-                </div>
-              </div>
-            </Col>
-          </Row>
-          <Row className="d-flex justify-content-around w-100 mb-3 gy-2">
-            <Col sm={12} md={6} lg={3}>
-              <Button className="w-100 py-2 myButton" variant="primary">
-                Disponibile per
-              </Button>
-            </Col>
-            <Col sm={12} md={6} lg={3}>
-              <Button className="w-100 py-2 myButton" variant="outline-primary">
-                Aggiungi sezione del profilo
-              </Button>
-            </Col>
-            <Col sm={12} md={6} lg={3}>
-              <Button className="w-100 py-2 myButton" variant="outline-secondary">
-                Risorse
-              </Button>
-            </Col>
-            <Col sm={12} md={6} lg={3}>
-              <Button className="w-100 py-2 myButton" variant="outline-primary">
-                Migliora profilo
-              </Button>
-            </Col>
-          </Row>
+              </Col>
+            </Row>
+            <Row className="d-flex justify-content-around w-100 mb-3 gy-2">
+              <Col sm={12} md={6} lg={3}>
+                <Button className="w-100 py-2 myButton" variant="primary">
+                  Disponibile per
+                </Button>
+              </Col>
+              <Col sm={12} md={6} lg={3}>
+                <Button className="w-100 py-2 myButton" variant="outline-primary">
+                  Aggiungi sezione del profilo
+                </Button>
+              </Col>
+              <Col sm={12} md={6} lg={3}>
+                <Button className="w-100 py-2 myButton" variant="outline-secondary">
+                  Risorse
+                </Button>
+              </Col>
+              <Col sm={12} md={6} lg={3}>
+                <Button className="w-100 py-2 myButton" variant="outline-primary">
+                  Migliora profilo
+                </Button>
+              </Col>
+            </Row>
 
-          <Row>
-            <Col>
-              <Alert variant="primary">
-                <div className="d-flex justify-content-between align-items-center">
-                  <h5>Disponibile a lavorare</h5>
-                  <Pencil size={20} />
-                </div>
-                <p className="mb-0">Ruoli di </p>
-                <p className="mb-0">Mostra dettagli</p>
-              </Alert>
-            </Col>
-            <Col>
-              <Alert variant="light" dismissible>
-                <h5>Metti in risalto i tuoi servizi</h5>
-                <p className="mb-0">in un'apposita sezione del tuo profilo, così sarà più facile trovarti.</p>
-                <p className="mb-0">Inizia</p>
-              </Alert>
-            </Col>
+            <Row>
+              <Col>
+                <Alert variant="primary">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <h5>Disponibile a lavorare</h5>
+                    <Pencil size={20} />
+                  </div>
+                  <p className="mb-0">Ruoli di </p>
+                  <p className="mb-0">Mostra dettagli</p>
+                </Alert>
+              </Col>
+              <Col>
+                <Alert variant="light" dismissible>
+                  <h5>Metti in risalto i tuoi servizi</h5>
+                  <p className="mb-0">in un'apposita sezione del tuo profilo, così sarà più facile trovarti.</p>
+                  <p className="mb-0">Inizia</p>
+                </Alert>
+              </Col>
+            </Row>
           </Row>
-        </Row>
-      </Card.Body>
-    </Card>
+        </Card.Body>
+      </Card>
+      <InfoSections header="Informazioni" title="Nome Cognome" subtitle="Dettagli Personali" details="Lorem ipsum" image={Graphic} />
+      <InfoSections header="Esperienza" title="Mansione" subtitle="Tipo di lavoro" details="durata" image={Graphic} />
+      <InfoSections header="Formazione" title="Scuola/università" subtitle="durata" details="Votazione" image={Graphic} />
+      <InfoSections header="Competenze" title="Disciplina" subtitle="Scuola/università" details="Altre informazioni" image={Graphic} />
+    </>
   );
 };
 
