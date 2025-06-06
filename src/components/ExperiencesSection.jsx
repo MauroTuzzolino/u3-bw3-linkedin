ebd908dde05158a6be25f7a7c5ea09e19e16fe10
ZairaStra <straticozaira@gmail.com>
2025-06-06 16:14:31 +0200

    spaziatura immagini esperienza

--- src/components/ExperiencesSection.jsx
+++ src/components/ExperiencesSection.jsx
@@ -62,7 +62,7 @@ const ExperiencesSection = ({ userId, isMyProfile }) => {
           {experiences && experiences.length > 0 ? (
             experiences.map((exp) => (
               <Row key={exp._id} className="mb-3 border-bottom">
-                <Col sm={1} className="d-none d-md-block">
+                <Col sm={1} className="d-none d-md-block me-4 ">
                   <Image src={exp.image} className="companyImgExperience" />
                 </Col>
                 <Col>
